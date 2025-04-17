e532a5213e3383acd963aa181f120a10c1a503b5
Kyal Lanum <kyallanum@gmail.com>
2025-04-16 20:21:49 -0600

    Adds a notification window for non-premium notification

--- lua/smm/api/api.lua
+++ lua/smm/api/api.lua
@@ -141,4 +141,38 @@ function M.resume_track(position_ms, auth_info, callback)
   end)
 end
 
+---@param auth_info Auth_Info
+---@param callback fun(response_body: table, response_headers: table, status_code: integer)
+---@return nil
+function M.get_user_profile(auth_info, callback)
+  if not auth_info or not auth_info.access_token or not auth_info.token_type then
+    error 'You must supply an access_token and token type to run get_user_profile'
+  end
+
+  local headers = {
+    ['Authorization'] = auth_info.token_type .. ' ' .. auth_info.access_token,
+    ['Content-Type'] = 'application/json',
+  }
+
+  http.get('https://api.spotify.com/v1/me', headers, nil, function(response_body, response_headers, status_code)
+    -- Handle 401 Unauthorized by refreshing the token
+    if status_code == 401 and auth_info.refresh_token then
+      auth_info = auth.refresh_access_token(auth_info.refresh_token)
+      if not auth_info or not auth_info.access_token then
+        callback(nil, nil, 401)
+        return
+      end
+
+      -- Retry with new token
+      local new_headers = {
+        ['Authorization'] = auth_info.token_type .. ' ' .. auth_info.access_token,
+      }
+
+      http.get('https://api.spotify.com/v1/me', new_headers, nil, callback)
+      return
+    end
+    callback(response_body, response_headers, status_code)
+  end)
+end
+
 return M
--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,13 +1,18 @@
 vim.api.nvim_create_user_command('Spotify', function()
   local auth = require 'smm.auth.oauth'
   local controller = require 'smm.controller.controller'
+  vim.notify('Imported dependencies', vim.log.levels.INFO)
 
   if not controller.auth_info then
     local auth_info
     local new_auth_info = auth.ensure_valid_token(auth_info)
+    vim.notify('got valid auth token', vim.log.levels.INFO)
     if new_auth_info then
       auth_info = new_auth_info
+      vim.notify('starting playback', vim.log.levels.INFO)
       controller.start_playback(auth_info)
+      require('smm.controller.controller').get_profile_type()
+      vim.notify('received profile type', vim.log.levels.INFO)
     else
       vim.notify('Unable to authenticate', vim.log.levels.ERROR)
     end
