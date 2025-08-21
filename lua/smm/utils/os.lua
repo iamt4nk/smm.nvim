ce6fa67eccfa3e94be5f0f1f19f9e13cd41f7d2e
Kyal Lanum <kyallanum@gmail.com>
2025-08-21 11:44:47 -0600

    Fixes URL opening. Looks like we only need to shellescape for Windows/WSL

--- lua/smm/utils/os.lua
+++ lua/smm/utils/os.lua
@@ -2,10 +2,8 @@ local logger = require 'smm.utils.logger'
 local M = {}
 
 function M.open_browser(url)
-  local escaped_url = vim.fn.shellescape(url)
-
   logger.debug 'Opening browser'
-  local platform = vim.loop.os_uname().sysname
+  local platform = vim.uv.os_uname().sysname
   logger.debug('Platform: %s', platform)
 
   local uname = vim.fn.system 'uname -r'
@@ -15,22 +13,24 @@ function M.open_browser(url)
 
   if is_wsl then
     logger.debug 'Opening powershell process and spawning browser'
+    local escaped_url = vim.fn.shellescape(url)
+    logger.debug('Escaped URL: %s', escaped_url)
     vim.fn.system { 'powershell.exe', '/c', 'start', escaped_url }
     return
   end
 
   if platform == 'Darwin' then
-    vim.fn.system { 'open', escaped_url }
+    vim.fn.system { 'open', url }
     return
   end
 
   if platform == 'Linux' then
-    vim.fn.system { 'xdg-open', escaped_url }
+    vim.fn.system { 'xdg-open', url }
     return
   end
 
   if platform == 'Windows' then
-    vim.fn.system { 'cmd', '/c', 'start', escaped_url }
+    vim.fn.system { 'cmd', '/c', 'start', url }
     return
   end
 end
